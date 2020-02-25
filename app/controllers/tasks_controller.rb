e7880aead4d634f2b105ec81ff2be77a2e512b32
Charles Buxton <chazbux@gmail.com>
2020-02-25 14:06:28 -0700

    added user_id to tasks table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,40 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit]
+  
   def index
-  end
-
-  def new
+    @tasks = current_user.tasks
   end
 
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      #flash message
+      redirect_to accounts_path
+    else
+      #flash message
+      render :new
+    end
+  end
+
   def edit
   end
+
+  private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_203007) do
+ActiveRecord::Schema.define(version: 2020_02_25_210458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_25_203007) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
   end
 
   create_table "users", force: :cascade do |t|
